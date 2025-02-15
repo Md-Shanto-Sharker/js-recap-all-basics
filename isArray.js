f1cc7e689a07a64849e2c5fa2a4969de4d7f4de9
SHANTO <rabbymd55555@gmail.com>
2025-02-15 19:21:43 +0600

    added isArray.js file

---
+++ isArray.js
@@ -0,0 +1,6 @@
+const friends = ['shanto','sharker','irfan','rasel'];
+const number = '';
+const food = [];
+console.log(Array.isArray(friends));
+console.log(Array.isArray(number));
+console.log(Array.isArray(food));
