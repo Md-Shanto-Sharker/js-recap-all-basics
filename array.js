1e795c1ca0048c7af9b29c0b597e80cd5cd48fe0
SHANTO <rabbymd55555@gmail.com>
2025-02-15 18:37:35 +0600

    added first file in array

---
+++ array.js
@@ -0,0 +1,15 @@
+// array length>>>>>>>>>>>>>>\
+const number = [12,354,544,3,5,6,77,34,12,354,544,3,5,6,77,34,12,354,544,3,5,6,77,34]
+console.log(number.length);
+// index of array?????????????????
+console.log(number[0]);
+console.log(number[1]);
+console.log(number[2]);
+console.log(number[5]);
+
+const index = number[10];
+console.log(index);
+
+// set or update index value;?????????????????????
+number[10]=0;
+console.log(number);
