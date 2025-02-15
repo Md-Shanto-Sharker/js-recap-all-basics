51b7604170df328a8d502dcfd1467bc53b2f477f
SHANTO <rabbymd55555@gmail.com>
2025-02-15 19:38:50 +0600

    added task-4.js file

---
+++ task-4.js
@@ -0,0 +1,6 @@
+const number = [];
+const friends = ['shanto','sharker','irfan'];
+const array='';
+console.log(Array.isArray(number));
+console.log(Array.isArray(friends));
+console.log(Array.isArray(array));
