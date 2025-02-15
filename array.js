c379a7d8de6abf61af6e6c7cbefa27b9bb919b5f
SHANTO <rabbymd55555@gmail.com>
2025-02-15 19:04:41 +0600

    added push,pop etc

--- array.js
+++ array.js
@@ -1,15 +1,47 @@
 // array length>>>>>>>>>>>>>>\
-const number = [12,354,544,3,5,6,77,34,12,354,544,3,5,6,77,34,12,354,544,3,5,6,77,34]
-console.log(number.length);
+// const number = [12,354,544,3,5,6,77,34,12,354,544,3,5,6,77,34,12,354,544,3,5,6,77,34];
+// console.log(number.length);
 // index of array?????????????????
-console.log(number[0]);
-console.log(number[1]);
-console.log(number[2]);
-console.log(number[5]);
+// console.log(number[0]);
+// console.log(number[1]);
+// console.log(number[2]);
+// console.log(number[5]);
 
-const index = number[10];
-console.log(index);
+// const index = number[10];
+// console.log(index);
 
 // set or update index value;?????????????????????
-number[10]=0;
-console.log(number);
+// number[10]=0;
+// console.log(number);
+
+// use of push-pop()??????????????
+// const number = [12,354,544,34];
+// console.log(number);
+// number.push(10);
+// console.log(number);
+// console.log(number.push(10));
+// const result = number.push(10);
+// console.log(result);
+
+
+// use the pop()>>>>>>>>>>>>>>>>>>>>>>
+// const number2 = [12,354,544,34];
+// number2.pop(34);
+// console.log(number2);
+// number2.pop([3]);
+// console.log(number2);
+
+
+///use the shift()>>>>>>>>>>>>>>>>
+const number3 = [10,22,44,88];
+console.log(number3);
+number3.shift(10);
+console.log(number3);
+number3.shift([2]);
+console.log(number3);
+
+// use the unshift()>>>>>>>>>>>>>
+const number4 = [20,32,4,99];
+console.log(number3);
+number3.unshift(10);
+console.log(number3);
