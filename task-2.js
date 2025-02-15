0c60bab540ad920e49f7099869f14b4662598696
SHANTO <rabbymd55555@gmail.com>
2025-02-15 19:32:29 +0600

    added task-2.js file

---
+++ task-2.js
@@ -0,0 +1,8 @@
+const array = ['banderban','kuyakata','khagrasori'];
+console.log(array);
+array.push('see-beach')
+console.log(array);
+array.push('centmartin','sonergown');
+console.log(array);
+array.pop();
+console.log(array);
