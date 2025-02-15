0635ff64e1fb75d0bc2c20c01762620841d4331c
SHANTO <rabbymd55555@gmail.com>
2025-02-15 19:10:52 +0600

    added includes.js file

---
+++ includes.js
@@ -0,0 +1,9 @@
+const friends = ['shanto','sharker','irfan','rasel'];
+console.log(friends.includes('shanto'));
+console.log(friends.includes('santo'));
+
+if(friends.includes('sharker')){
+    console.log('yeah!');
+}else{
+    console.log("Noh");
+}
